6716f108cd82cd11e1619519edc08d1ba490a5b8
Tomas Krinickas <tomas.krinickas2@gmail.com>
2022-12-06 18:42:40 +0200

    2 lecture setup

--- Pirma paskaita/index.js
+++ Pirma paskaita/index.js
@@ -1,5 +1,17 @@
 const casual = require("casual");
 let random = Math.floor(Math.random() * 10) + 1;
-let randomNode = Math.floor(casual.random * 10) + 1;
+// let randomNode = Math.floor(casual.random * 10) + 1;
 
-console.log(casual.city, random, randomNode, casual.name_prefix, casual.first_name, casual.last_name);
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + 1) + min
+}
+
+// const name = "Rokas";
+
+// const surname = "Andreikenas";
+
+// const fullName = name + " " + surname; // bad
+
+// const newFullName = `${name} ${surname}` // good
+
+console.log(casual.city, random, casual.integer(1, 10), `${casual.name_prefix} ${casual.first_name} ${casual.last_name}`);
